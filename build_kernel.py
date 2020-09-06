f04622323e06bd10e2927f337e3cec1c3d9576fb
Mike Kaliman <kaliman.mike@gmail.com>
2020-09-06 13:06:20 -0400

    Auto-format unsure if source dir deletion works

--- build_kernel.py
+++ build_kernel.py
@@ -15,10 +15,13 @@ MAX_VERSIONS_TO_KEEP = 2
 KERNEL_BUILD_DIR = Path("/usr/src/linux")
 INSTALL_PATH = Path("/boot/EFI/Gentoo")
 
+
 def error_and_exit(error):
     """ print error then exit with return code 1 """
-    print(Fore.RED + f"{sys.argv[0]}: " + Style.RESET_ALL + f"Error! {error}, exiting...")
+    print(Fore.RED + f"{sys.argv[0]}: " +
+          Style.RESET_ALL + f"Error! {error}, exiting...")
     sys.exit(1)
+
 
 def script_info(info):
     """ print debugging info """
@@ -27,14 +30,17 @@ def script_info(info):
 
 class VersionInfo:
     """ Container for organizing all the kernel versions and files """
+
     def __init__(self, version_triple: str, vmlinuz: Path, system_map: Path, config: Path, is_old: bool):
         self.version_triple = version_triple
         self.vmlinuz = vmlinuz
         self.system_map = system_map
         self.config = config
         self.is_old = is_old
+
     def __repr__(self):
         return "VersionInfo()"
+
     def __str__(self):
         if self.is_old:
             return f"VersionInfo({self.version_triple}.old, {self.vmlinuz}, {self.system_map}, {self.config})"
@@ -43,14 +49,14 @@ class VersionInfo:
 
     def as_tuple(self):
         """ Convert VersionInfo into a tuple """
-        
+
         # Indices:
         # 0 1 2 3 4
         # X . Y . Z
         version_triple_split = self.version_triple.split(sep=".")
-        major = version_triple_split[0] # X
-        minor = version_triple_split[1] # Y
-        patch = version_triple_split[2] # Z
+        major = version_triple_split[0]  # X
+        minor = version_triple_split[1]  # Y
+        patch = version_triple_split[2]  # Z
         if self.is_old:
             # is .old, penalize it in the sorting by making this -1
             old_adj = -1
@@ -61,21 +67,22 @@ class VersionInfo:
 
     def __eq__(self, other):
         return self.as_tuple() == other.as_tuple()
-    
+
     def __ne__(self, other):
         return self.as_tuple() != other.as_tuple()
-    
+
     def __gt__(self, other):
         return self.as_tuple() > other.as_tuple()
-    
+
     def __ge__(self, other):
         return self > other or self == other
-    
+
     def __lt__(self, other):
         return self.as_tuple() < other.as_tuple()
-    
+
     def __le__(self, other):
         return self < other or self == other
+
 
 class KernelUpdater:
     current_kernels: [VersionInfo] = []
@@ -96,11 +103,12 @@ class KernelUpdater:
         os.chdir(str(KERNEL_BUILD_DIR))
 
         # Could get running config from /proc/config.gz but I'll just copy the newest one in /boot
-        src = INSTALL_PATH / self.current_kernels[0].config # The newest config we have
+        # The newest config we have
+        src = INSTALL_PATH / self.current_kernels[0].config
         dest = Path(os.getcwd() + "/.config")
 
         script_info(f"Copying {src.absolute()} to {dest.absolute()}")
-        shutil.copy(str(src), str(dest))
+        shutil.copy(src, dest)
 
         script_info(f"syncing configuration to new kernel in {os.getcwd()}")
         try:
@@ -113,7 +121,8 @@ class KernelUpdater:
         os.chdir(str(KERNEL_BUILD_DIR))
         script_info(f"Compiling kernel in {os.getcwd()}")
         try:
-            subprocess.run(["/bin/bash", "-c", "\"make\" --jobs $(nproc) --load-average $(nproc)"], check=True)
+            subprocess.run(
+                ["/bin/bash", "-c", "\"make\" --jobs $(nproc) --load-average $(nproc)"], check=True)
         except CalledProcessError as err:
             error_and_exit(err)
 
@@ -127,12 +136,13 @@ class KernelUpdater:
         except CalledProcessError as err:
             error_and_exit(err)
 
-        script_info(f"Installing kernel image, system map, and config to {str(INSTALL_PATH)}")
+        script_info(
+            f"Installing kernel image, system map, and config to {str(INSTALL_PATH)}")
         try:
             # This copies over the system environment but appends the INSTALL_PATH variable
             # needed during "make install"
             subprocess.run(["/bin/bash", "-c", "\"make\" install"],
-                        env=dict(os.environ, INSTALL_PATH=str(INSTALL_PATH)), check=True)
+                           env=dict(os.environ, INSTALL_PATH=str(INSTALL_PATH)), check=True)
         except CalledProcessError as err:
             error_and_exit(err)
 
@@ -154,7 +164,8 @@ class KernelUpdater:
         # Reset current list
         self.current_kernels = []
         os.chdir(str(INSTALL_PATH))
-        script_info(f"Cleaning up old kernels in {os.getcwd()}")
+        script_info(
+            f"Searching for installed kernel files in {os.getcwd()}...")
 
         # Find all vmlinuz, config, and system map files
         vmlinuzes = list(Path().glob("vmlinuz-*-gentoo*"))
@@ -164,13 +175,15 @@ class KernelUpdater:
         # Ensure that the 3 lists are the same length
         if not len(list(vmlinuzes)) == len(list(system_maps)) and len(list(system_maps)) == len(list(configs)):
             error_and_exit(f"There are {len(list(vmlinuzes))} vmlinuz files, {len(list(system_maps))} "
-                        + f"system maps, and {len(list(configs))} config files")
+                           + f"system maps, and {len(list(configs))} config files")
 
         for vmlinuz in vmlinuzes:
-            version_triple = str(vmlinuz).split(sep="-") # Split up the vmlinuz string
+            version_triple = str(vmlinuz).split(
+                sep="-")  # Split up the vmlinuz string
             if len(version_triple) != 3:
-                error_and_exit(f"Expected len(version) == 3, got {len(version_triple)}!")
-            version_triple = version_triple[1] # extract the X.Y.Z
+                error_and_exit(
+                    f"Expected len(version) == 3, got {len(version_triple)}!")
+            version_triple = version_triple[1]  # extract the X.Y.Z
 
             is_old = bool(vmlinuz.suffix == '.old')
             # Find the accompying System.map and config
@@ -182,26 +195,31 @@ class KernelUpdater:
                 # to see if it has our version_triple and if the filename ends with .old
                 # Note: .pop() is a lazy way to convert a single-element list to the underlying type
                 # Ensure that the suffix is .old
-                system_map = [s for s in system_maps if version_triple in str(s) and str(s).endswith(".old")].pop()
-                config = [s for s in configs if version_triple in str(s) and str(s).endswith(".old")].pop()
+                system_map = [s for s in system_maps if version_triple in str(
+                    s) and str(s).endswith(".old")].pop()
+                config = [s for s in configs if version_triple in str(
+                    s) and str(s).endswith(".old")].pop()
             else:
                 # Ensure that the suffix isn't .old
-                system_map = [s for s in system_maps if version_triple in str(s) and not str(s).endswith(".old")].pop()
-                config = [s for s in configs if version_triple in str(s) and not str(s).endswith(".old")].pop()
+                system_map = [s for s in system_maps if version_triple in str(
+                    s) and not str(s).endswith(".old")].pop()
+                config = [s for s in configs if version_triple in str(
+                    s) and not str(s).endswith(".old")].pop()
 
-            self.current_kernels.append(VersionInfo(version_triple, vmlinuz, system_map, config, is_old))
-        
+            self.current_kernels.append(VersionInfo(
+                version_triple, vmlinuz, system_map, config, is_old))
+
         self.current_kernels = sorted(self.current_kernels, reverse=True)
-
 
     def __clean_up(self):
         """ delete old kernels """
-        
+
         self.__find_installed_kernels()
 
-        # If there's MAX_VERSIONS_TO_KEEP or less versions, exit 
+        # If there's MAX_VERSIONS_TO_KEEP or less versions, exit
         if len(self.current_kernels) <= MAX_VERSIONS_TO_KEEP:
-            script_info(f"Only {len(self.current_kernels)} kernels are there, not deleting any")
+            script_info(
+                f"Only {len(self.current_kernels)} kernels are there, not deleting any")
             return
 
         # Otherwise delete everything but the 2 newest versions
@@ -218,34 +236,45 @@ class KernelUpdater:
         for _ in range(num_to_delete):
             kernel_to_delete = self.current_kernels.pop()
             if kernel_to_delete.is_old:
-                script_info(f"Deleting version {kernel_to_delete.version_triple}.old")
+                script_info(
+                    f"Deleting version {kernel_to_delete.version_triple}.old")
             else:
-                script_info(f"Deleting version {kernel_to_delete.version_triple}")
+                script_info(
+                    f"Deleting version {kernel_to_delete.version_triple}")
 
             # Evidently "unlink" is the same as remove
-            script_info(f"Deleting file {str(kernel_to_delete.vmlinuz.absolute())}")
+            script_info(
+                f"Deleting file {str(kernel_to_delete.vmlinuz.absolute())}")
             kernel_to_delete.vmlinuz.unlink()
-            script_info(f"Deleting file {str(kernel_to_delete.system_map.absolute())}")
+            script_info(
+                f"Deleting file {str(kernel_to_delete.system_map.absolute())}")
             kernel_to_delete.system_map.unlink()
-            script_info(f"Deleting file {str(kernel_to_delete.config.absolute())}")
+            script_info(
+                f"Deleting file {str(kernel_to_delete.config.absolute())}")
             kernel_to_delete.config.unlink()
+
+            source_dir = Path(
+                f"{KERNEL_BUILD_DIR}/linux-{kernel_to_delete.version_triple}-gentoo")
+            script_info(f"Deleting source directory {str(source_dir)}")
+            shutil.rmtree(source_dir)
 
     def update(self):
         """ Run all of the private methods in the proper order """
-        self.__update_config()
-        self.__compile_kernel()
-        self.__install_new_kernel()
-        self.__recompile_extra_modules()
+        # self.__update_config()
+        # self.__compile_kernel()
+        # self.__install_new_kernel()
+        # self.__recompile_extra_modules()
         self.__clean_up()
 
 
 def main():
     """ main """
-    init() # Init colorama, not necessarily needed for Linux but why not
+    init()  # Init colorama, not necessarily needed for Linux but why not
     script_info("-----------------------")
     updater = KernelUpdater()
     updater.update()
     script_info("-----------------------")
+
 
 if __name__ == '__main__':
     main()
