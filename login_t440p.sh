839e086ad3eb74e359748ffe854e5cd7716d899a
mik90 <kaliman.mike@gmail.com>
2021-01-25 18:26:00 -0500

    Fix var assignment and use abs path

--- login_t440p.sh
+++ login_t440p.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-NOTES_DIR = "~/OneDrive"
+NOTES_DIR="/home/mike/OneDrive"
 if [ ! "$(ls -A $NOTES_DIR)" ]; then
   # Directory is empty, so mount OneDrive
   rclone --vfs-cache-mode writes mount onedrive: $NOTES_DIR &
