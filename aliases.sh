1ca843fbd69a030c1d639f48a898f8db052bd05e
Mike Kaliman <kaliman.mike@gmail.com>
2021-03-04 21:15:38 -0500

    Add common ls aliases

--- aliases.sh
+++ aliases.sh
@@ -2,4 +2,6 @@ alias notes="vim /home/mike/OneDrive/notes.md"
 # Overwrite the local dir, rely on remote being valid
 alias syncnotes="rclone sync -i onedrive:/ /home/mike/OneDrive/"
 alias ls="ls --color=auto"
+alias ll="ls --color=auto -l"
+alias la="ll --color=auto --all"
 
