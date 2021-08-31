56d0cf263d8fb89516a9acbc8440a02573b563bf
Mike Kaliman <kaliman.mike@gmail.com>
2021-08-31 17:52:39 -0400

    Use obsidian for notes and sync

--- login_t440p.sh
+++ login_t440p.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-NOTES_DIR="/home/mike/OneDrive"
-if [ ! "$(ls -A $NOTES_DIR)" ]; then
+#NOTES_DIR="/home/mike/OneDrive"
+#if [ ! "$(ls -A $NOTES_DIR)" ]; then
   # Directory is empty, so mount OneDrive
-  rclone --vfs-cache-mode writes mount onedrive: $NOTES_DIR &
-fi
+#  rclone --vfs-cache-mode writes mount onedrive: $NOTES_DIR &
+#fi
 
-sudo /home/mike/Development/scripts/set_t440p_backlight.sh 2300
+#sudo /home/mike/Development/scripts/set_t440p_backlight.sh 2300
 
