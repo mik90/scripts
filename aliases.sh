8fdfe4fece021f328f90f20aad5a991fcbec6b47
Mike Kaliman <kaliman.mike@gmail.com>
2021-11-23 21:31:34 -0500

    moved files to dotfiles repo

--- aliases.sh
+++
@@ -1,7 +0,0 @@
-alias notes="nvim /home/mike/OneDrive/Notes/notes.md"
-# Overwrite the local dir, rely on remote being valid
-alias syncnotes="rclone sync -i onedrive:/ /home/mike/OneDrive/"
-alias ls="ls --color=auto"
-alias ll="ls --color=auto -l"
-alias la="ll --color=auto --all"
-
