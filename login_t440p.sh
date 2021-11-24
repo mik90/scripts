7f2e90667c0619e65068a74060d7ea2b0afab359
Mike Kaliman <kaliman.mike@gmail.com>
2021-11-23 21:34:06 -0500

    Fix dir

--- login_t440p.sh
+++ login_t440p.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-#NOTES_DIR="/home/mike/OneDrive"
-#if [ ! "$(ls -A $NOTES_DIR)" ]; then
+ONEDRIVE_DIR="/home/mike/OneDrive"
+if [ ! "$(ls -A $ONEDRIVE_DIR)" ]; then
   # Directory is empty, so mount OneDrive
-#  rclone --vfs-cache-mode writes mount onedrive: $NOTES_DIR &
-#fi
+  rclone --vfs-cache-mode writes mount onedrive: $ONEDRIVE_DIR &
+fi
 
 #sudo /home/mike/Development/scripts/set_t440p_backlight.sh 2300
 
